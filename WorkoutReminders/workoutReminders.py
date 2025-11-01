8fbdb540ea13b5882ed6e5fa9cb03864e4b13ba6
jluk7 <jluk7@gatech.edu>
2025-11-01 00:12:37 -0400

    located chats.png and smile.png

--- WorkoutReminders/workoutReminders.py
+++ WorkoutReminders/workoutReminders.py
@@ -1,30 +1,10 @@
 # import pygetwindow as gw
-# import pyautogui
-# import win32gui
-# import win32con
-# import subprocess
-
-# subprocess.Popen(["cmd", "/C", "start whatsapp://send?phone=19178866195"], shell=True)
-
+import pyautogui
+import time
 import os
 
 os.startfile("whatsapp://send?phone=19176126688")
-
-# def bring_to_front(window_title):
-#     hwnd = win32gui.FindWindow(None, window_title)
-#     if hwnd:
-#         win32gui.SetForegroundWindow(hwnd)
-#         win32gui.ShowWindow(hwnd, win32con.SW_RESTORE) # Restore if minimized
-#     else:
-#         print(f"Window with title '{window_title}' not found.")
-
-
-# bring_to_front("WhatsApp") # Replace with your window's title
-
-# windows = gw.getAllWindows()
-
-# for window in windows:
-#     print("activating "+ window.title)
-#     if window.title == "WhatsApp":
-#         bring_to_front(window.title) # Replace with your window's title
-#         break
+time.sleep(5)
+searchBoxPic = "Data\\chats.png"
+searchBoxLoc = pyautogui.locateCenterOnScreen(searchBoxPic)
+pyautogui.click(searchBoxLoc)
