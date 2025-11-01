d6c4e535f3b82bc9522f9b3d04c1986a4cc3abf2
jluk7 <jluk7@gatech.edu>
2025-11-01 00:21:13 -0400

    use ctrl+f to find searchbox

--- WorkoutReminders/workoutReminders.py
+++ WorkoutReminders/workoutReminders.py
@@ -5,6 +5,11 @@ import os
 
 os.startfile("whatsapp://send?phone=19176126688")
 time.sleep(5)
-searchBoxPic = "Data\\chats.png"
-searchBoxLoc = pyautogui.locateCenterOnScreen(searchBoxPic)
-pyautogui.click(searchBoxLoc)
+# searchBoxPic = "Data\\chats.png"
+# searchBoxLoc = pyautogui.locateCenterOnScreen(searchBoxPic)
+# pyautogui.click(searchBoxLoc)
+
+pyautogui.keyDown("ctrl")
+pyautogui.press('f')
+pyautogui.keyUp("ctrl")
+pyautogui.write("Woodmont Workouts")
