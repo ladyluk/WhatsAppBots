a214d2a007a3a1eef1d0b481b7a974a178dfafa4
jluk7 <jluk7@gatech.edu>
2025-11-01 00:33:42 -0400

    select target chat

--- WorkoutReminders/workoutReminders.py
+++ WorkoutReminders/workoutReminders.py
@@ -5,11 +5,22 @@ import os
 
 os.startfile("whatsapp://send?phone=19176126688")
 time.sleep(5)
-# searchBoxPic = "Data\\chats.png"
-# searchBoxLoc = pyautogui.locateCenterOnScreen(searchBoxPic)
-# pyautogui.click(searchBoxLoc)
+
+
 
 pyautogui.keyDown("ctrl")
 pyautogui.press('f')
 pyautogui.keyUp("ctrl")
+# clears textbox
+pyautogui.hotkey('ctrl', 'a')
+pyautogui.press('delete')
+
+# search for workout class
 pyautogui.write("Woodmont Workouts")
+
+# click into appropriate chat
+pyautogui.press('tab')
+pyautogui.press('enter')
+
+# write message
+
